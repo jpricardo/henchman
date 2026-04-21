9e6143e1dba9dc323957eef41becce1de9d0eb45
jpricardo <joaopds.ricardo@gmail.com>
2026-04-21 19:10:11 -0300

    feat(#1): overhead const assertion

--- internal/cache/entry_test.go
+++ internal/cache/entry_test.go
@@ -1,12 +1,24 @@
 package cache
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestEntryStructOverhead_OverheadSize(t *testing.T) {
+	e := Entry{}
+	us := unsafe.Sizeof(e)
+
+	if us != entryStructOverhead {
+		t.Errorf("Struct size %d doesn't match overhead constant %d", us, entryStructOverhead)
+	}
+}
 
 func TestSizeBytes_OverheadSize(t *testing.T) {
 	e := Entry{}
 	s := e.SizeBytes()
 
 	if s != int64(entryStructOverhead) {
-		t.Errorf("Struct size %d doesn't match overhead constant %d", s, entryStructOverhead)
+		t.Errorf("Computed size %d doesn't match overhead constant %d", s, entryStructOverhead)
 	}
 }
